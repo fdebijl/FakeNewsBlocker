6521ddece94f50b3dcd8372f1dee1e766b7e615b
Fdebijl <floris.debijl@live.nl>
2017-01-18 08:23:51 +0100

    Correctielink verwijst nu naar Github ipv Twitter

--- messenger.js
+++ messenger.js
@@ -7,15 +7,16 @@
 	btn2.title = "Ja, hartstikke nep";
 	var opt = {
 		type: "basic",
-		title: "Deze website brengt mogelijk fake news!",
-		message: "Volgens onze gegevens brengt deze website mogelijk fake news.",
+		title: "Fake News",
+		message: "Volgens onze gegevens brengt deze website mogelijk fake news. ",
 		iconUrl: "logo128_fake.png",
 		buttons: [btn1, btn2]
 	};
 	
 	function btnClick(notid, btninx) {
 		if (btninx === 0) {
-			window.open("https://twitter.com/intent/tweet?text=%40fdebijl%20Hey,%20de%20pagina%20op%20LINKHIER%20is%20geen%20fake%20news%20:D","_blank");
+			window.open("https://github.com/Fdebijl/FakeNewsBlocker/issues");
+			chrome.notifications.clear(notid);
 		} else {
 			chrome.notifications.clear(notid);
 		}
